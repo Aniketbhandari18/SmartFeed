5612c0603153f46e3f4b175330886af260561b40
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-08-07 21:51:16 +0530

    feat: add trim validation to spaceFormSchema fields

--- src/lib/zodSchemas/spaceFormSchema.ts
+++ src/lib/zodSchemas/spaceFormSchema.ts
@@ -3,16 +3,19 @@ import { z } from "zod";
 export const spaceFormSchema = z.object({
   name: z
     .string()
+    .trim()
     .min(3, "Name must be at least 3 characters.")
     .max(30, "Name must be at most 30 characters."),
 
   title: z
     .string()
+    .trim()
     .min(3, "Title must be at least 3 characters.")
     .max(30, "Title must be at most 30 characters."),
 
   description: z
     .string()
+    .trim()
     .min(10, "Description must be at least 10 characters.")
     .max(500, "Description must be at most 500 characters."),
 
