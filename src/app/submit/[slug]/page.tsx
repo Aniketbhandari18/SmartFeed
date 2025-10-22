19cd40211ffb208c7acf9c9a1500132b812bd59c
Aniket Bhandari <aniketbhandari1807@gmail.com>
2025-10-22 22:14:13 +0530

    fix: update Page component to use async/await for params

--- src/app/submit/[slug]/page.tsx
+++ src/app/submit/[slug]/page.tsx
@@ -1,12 +1,11 @@
 import SubmitFeedbackPage from "@/components/pages/SubmitFeedbackPage";
-import { use } from "react";
 
-export default function Page({
+export default async function Page({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const { slug } = use(params);
+  const { slug } = await params;
   console.log("slug:", slug);
 
   return <SubmitFeedbackPage slug={slug} />;
